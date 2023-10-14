f3ec5c2ededf1d6a8af4bd269efe8889e694b783
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-15 00:01:53 +0530

    Add files via upload

---
+++ 05_While_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 0;
+    while (n != 10)
+    {
+        cout << "Happy New Year " << endl;
+        n++;
+    }
+
+    return 0;
+}
