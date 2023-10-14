dbb96ff01a18df2a001940bfd50661bcbb25a6f9
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-14 23:33:54 +0530

    Add files via upload

---
+++ 03_taking_input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1, n2, sum;
+    cout << "Enter the first number " << endl;
+    cin >> n1;
+    cout << "Enter the second number " << endl;
+    cin >> n2;
+    sum = n1 + n2;
+    cout << "The sum of the two numbers n1 and n2 is " << sum << endl;
+
+    return 0;
+}
